055e21861d2f5b0ae2ebb8f67487b923a6b28e58
小池 <r.koike@tcmobile.jp>
2018-05-25 17:43:26 +0900

    とりあえず検索機能

--- SearchHashTag.php
+++ SearchHashTag.php
@@ -1,1 +1,21 @@
 <?php
+
+// twistoauth の読み込み
+require 'TwistOAuth.phar';
+
+// 設定したやーつー
+$consumer_key = 'xxxxxxxxxxx';
+$consumer_secret = 'xxxxxxxxxxx';
+$access_token = 'xxxxxxxxxxx';
+$access_token_secret = 'xxxxxxxxxxx';
+
+// 検索条件
+$query = '#xxxxxxxxxx';
+$count = '100';
+$lang = 'ja';
+
+$connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+
+// ハッシュタグによるツイート検索
+$hash_params = ['q' => $query ,'count' => $count, 'lang'=> $ja];
+$hash = $connection->get('search/tweets', $hash_params)->statuses;
