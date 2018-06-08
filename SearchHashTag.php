8411abacce01dbdf471bb7dad446d078b843a48d
小池 <r.koike@tcmobile.jp>
2018-06-08 17:45:33 +0900

    アクセスキー等を設定ファイルから取得するように変更

--- SearchHashTag.php
+++ SearchHashTag.php
@@ -3,16 +3,17 @@
 // twistoauth の読み込み
 require 'TwistOAuth.phar';
 
-// 設定したやーつー
-$consumer_key = 'xxxxxxxxxxx';
-$consumer_secret = 'xxxxxxxxxxx';
-$access_token = 'xxxxxxxxxxx';
-$access_token_secret = 'xxxxxxxxxxx';
+// 設定の読み込み
+$conf = parse_ini_file('/srv/batch/conf/conf.ini');
 
-// 検索条件
-$query = '#xxxxxxxxxx';
-$count = '100';
-$lang = 'ja';
+// 設定から色々取得
+$consumer_key = $conf['consumer_key'];
+$consumer_secret = $conf['consumer_secret'];
+$access_token = $conf['access_token'];
+$access_token_secret = $conf['access_token_secret'];
+$query = $conf['hash_tag'];
+$count = $conf['get_count'];
+$lang = $conf['lang'];
 
 $connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 
