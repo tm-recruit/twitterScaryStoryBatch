9733a110c9b74a03828e2335fe466bd697451b48
小池 <r.koike@tcmobile.jp>
2018-06-11 15:38:27 +0900

    とりあえず実装

--- SearchHashTag.php
+++ SearchHashTag.php
@@ -3,6 +3,10 @@
 // twistoauth の読み込み
 require 'TwistOAuth.phar';
 
+// CSVファイルの読み込み
+$file = '/srv/batch/result/SearchResult.csv';
+$data = file_get_contents($file);
+
 // 設定の読み込み
 $conf = parse_ini_file('/srv/batch/conf/conf.ini');
 
@@ -18,5 +22,22 @@ $lang = $conf['lang'];
 $connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 
 // ハッシュタグによるツイート検索
-$hash_params = ['q' => $query ,'count' => $count, 'lang'=> $ja];
-$hash = $connection->get('search/tweets', $hash_params)->statuses;
+$hash_params = ['q' => $query ,'count' => $count, 'lang'=> $lang];
+$tweets = $connection->get('search/tweets', $hash_params)->statuses;
+
+foreach ($tweets as $tweet) {
+	$timestamp = $tweet->created_at;
+	$user = $tweet->user;
+	$name = $user->name . '@' . $user->screen_name;
+	// "を""に変換
+	$text = str_replace('"', '""', $tweet->text);
+	$rt = $tweet->retweet_count;
+	$like = $tweet->favorite_count;
+	$sep = '","';
+	$record =  '"' . $timestamp . $sep . $name  . $sep . $text  . $sep . $rt  . $sep . $like . '"';
+
+	$data .= $record . "\n";
+}
+
+// CSVファイルへ書き込み
+file_put_contents ($file , $data);
